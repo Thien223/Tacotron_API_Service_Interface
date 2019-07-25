bd621bf9c7ff5099103f8a0e69867a83f8066805
thien <thien@locslab.com>
2019-07-25 11:36:36 +0900

    Initial commit

---
+++ Tacotron_API_Service_Interface/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Tacotron_API_Service_Interface project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tacotron_API_Service_Interface.settings')
+
+application = get_wsgi_application()
